640225cc7054590aa9b48818dd8f9df71f1dfc53
Jared2910 <jared291095@gmail.com>
2023-02-16 01:39:18 -0600

    Implementacion de animaciones en el sidebar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 // PrimeNG
 // A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 import {ButtonModule} from 'primeng/button';
+import {CarouselModule} from 'primeng/carousel';
 import {ImageModule} from 'primeng/image';
 import {RippleModule } from 'primeng/ripple';
 
@@ -13,6 +14,7 @@ import {RippleModule } from 'primeng/ripple';
 @NgModule({
   exports:[
     ButtonModule,
+    CarouselModule,
     ImageModule,
     RippleModule
   ]
