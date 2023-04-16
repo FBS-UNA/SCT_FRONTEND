5cef14354eb848e01391a69dd108e370e5ff9410
Biron Aguilar Rodríguez <71033592+Biron2465@users.noreply.github.com>
2023-04-16 15:55:50 -0600

    Feature_mostrar_usuarios closed #63 (#64)

--- src/app/SCT/pages/asignacion-roles/asignacion-roles.component.ts
+++ src/app/SCT/pages/asignacion-roles/asignacion-roles.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TableCols } from '../../interfaces/table.interface';
 import { Table } from 'primeng/table';
+import { Usuario } from 'src/app/auth/interfaces/auth.interface';
+import { UsuariosService } from '../../services/usuarios.service';
+
 
 @Component({
   selector: 'app-asignacion-roles',
@@ -12,6 +15,8 @@ export class AsignacionRolesComponent implements OnInit {
 
   loading!: boolean;
 
+  usuarios: Usuario[] = [];
+
   cols: TableCols[] = [
     { field: '', header: 'Cédula', style: 'width: 10%' },
     { field: '', header: 'Nombre', style: 'width: 10%' },
@@ -21,13 +26,27 @@ export class AsignacionRolesComponent implements OnInit {
     { field: '', header: 'Roles', style: 'width: 10%' },
   ]
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuariosService
+  ) { }
 
   ngOnInit(): void {
+    this.cargarUsuarios()
   }
   
   clear(table: Table) {
     table.clear();
+  }
+
+  cargarUsuarios() {
+    this.loading= true;
+    this.usuarioService.getUsuarios().subscribe(OK => {
+      if (OK) {
+        this.loading = false;
+        this.usuarios = this.usuarioService.usuarios;
+        console.log(this.usuarios)
+      }
+    });
   }
 
 }
