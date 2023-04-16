168c6edf775ae5fbb947b65420835f4142f48140
Biron Aguilar Rodríguez <biron2465@gmail.com>
2023-04-16 16:20:16 -0600

    Componente
    Se crea el componente que va a contener la tabla de asignacion de roles a usuarios

---
+++ src/app/SCT/components/tabla-asignacion-roles/tabla-asignacion-roles.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-asignacion-roles',
+  templateUrl: './tabla-asignacion-roles.component.html',
+  styles: [
+  ]
+})
+export class TablaAsignacionRolesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
