0d8ba6422daa47d755c96ec69ab8c5a7b4d3bd87
Biron Aguilar Rodríguez <biron2465@gmail.com>
2023-04-16 16:24:48 -0600

    Refactorización de código
    Se crea la tabla y se manda a llamar en la pantalla de asignación de roles

--- src/app/SCT/components/tabla-asignacion-roles/tabla-asignacion-roles.component.ts
+++ src/app/SCT/components/tabla-asignacion-roles/tabla-asignacion-roles.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../../services/usuarios.service';
+import { Table } from 'primeng/table';
+import { TableCols } from '../../interfaces/table.interface';
+import { Usuario } from 'src/app/auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-tabla-asignacion-roles',
@@ -8,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaAsignacionRolesComponent implements OnInit {
 
-  constructor() { }
+  loading!: boolean;
+
+  usuarios: Usuario[] = [];
+
+  cols: TableCols[] = [
+    { field: '', header: 'Cédula', style: 'width: 10%' },
+    { field: '', header: 'Nombre', style: 'width: 10%' },
+    { field: '', header: 'Primer Apellido', style: 'width: 11%' },
+    { field: '', header: 'Segundo Apellido', style: 'width: 12%' },
+    { field: '', header: 'Fecha de Nacimiento', style: 'width: 15%' },
+    { field: '', header: 'Roles', style: 'width: 10%' },
+  ]
+
+  constructor(
+    private usuarioService: UsuariosService
+  ) { }
 
   ngOnInit(): void {
+    this.cargarUsuarios()
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
+
+  cargarUsuarios() {
+    this.loading= true;
+    this.usuarioService.getUsuarios().subscribe(OK => {
+      if (OK) {
+        this.loading = false;
+        this.usuarios = this.usuarioService.usuarios;
+        console.log(this.usuarios)
+      }
+    });
   }
 
 }
