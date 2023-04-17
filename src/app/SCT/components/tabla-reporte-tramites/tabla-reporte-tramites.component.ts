244323ed21aa36c9c010b37e002783316ec1bc25
Jared2910 <jared291095@gmail.com>
2023-04-16 23:09:39 -0600

    Implementacion de la tabla de reportes close #43

--- src/app/SCT/components/tabla-reporte-tramites/tabla-reporte-tramites.component.ts
+++ src/app/SCT/components/tabla-reporte-tramites/tabla-reporte-tramites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TableCols } from '../../interfaces/table.interface';
+import { Reporte } from '../../interfaces/reporte.interface';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-tabla-reporte-tramites',
@@ -8,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaReporteTramitesComponent implements OnInit {
 
+  loading !: boolean;
+
+  reportes : Reporte[] = []
+
+  cols: TableCols[] = [
+    { field: '', header: 'Código', style: 'width: 10%' },
+    { field: '', header: 'Área', style: 'width: 10%' },
+    { field: '', header: 'Trámite', style: 'width: 10%' },
+    { field: '', header: 'Cliente', style: 'width: 10%' },
+    { field: '', header: 'Usuario', style: 'width: 10%' },
+    { field: '', header: 'Fecha', style: 'width: 10%' },
+
+  ];
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clear(table: Table){
+    table.clear();
   }
 
 }
