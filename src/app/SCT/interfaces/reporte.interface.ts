6177cc6889ad4060b9cb4c424ed0ab42afb228ff
Jared2910 <jared291095@gmail.com>
2023-04-20 05:25:33 -0600

    Interfaz del reporte cambiada

--- src/app/SCT/interfaces/reporte.interface.ts
+++ src/app/SCT/interfaces/reporte.interface.ts
@@ -1,8 +1,11 @@
 export interface Reporte{
-    ID_REGISTRO_TRAMITE  : string 
-    NOMBRE_AREA : string
-    NOMBRE_TRAMITE : string
-    NOMBRE_CLIENTE : string
-    NOMBRE_USUARIO : string
-    FECHA_Y_HORA : string
+    OK : boolean;
+    MSG ?: string;
+    ID_REGISTRO_TRAMITE  ?: string;
+    NOMBRE_AREA ?: string;
+    NOMBRE_TRAMITE ?: string;
+    NOMBRE_CLIENTE ?: string;
+    NOMBRE_USUARIO ?: string;
+    FECHA_Y_HORA ?: string;
+
 } 
