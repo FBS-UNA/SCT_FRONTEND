95f50f8ffbed252d158c757daf2c0344fe892c44
Jared2910 <jared291095@gmail.com>
2023-05-10 02:27:53 -0600

    Creacion de la pantalla Graficos

---
+++ src/app/SCT/pages/graficos/graficos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficos',
+  templateUrl: './graficos.component.html',
+  styles: [
+  ]
+})
+export class GraficosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
